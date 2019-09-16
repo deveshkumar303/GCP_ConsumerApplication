9456f0738c7a18a6795eeb5c02e5bbca04557ba0
rajkumar753753 <raj.kumar753753@gmail.com>
2019-09-15 23:55:23 -0400

    Consumer Application First Commit
    Skelton of Consumer Applictaion Rest API

---
+++ GCP_ConsumerApplication/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GCP_ConsumerApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GCP_ConsumerApplication.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		// GET api/values
+		[HttpGet]
+		public ActionResult<string> Get()
+		{
+			ConsumerDetails[] consumerDetails = new ConsumerDetails[2];
+			consumerDetails[0] = new ConsumerDetails { firstName = "Williams", lastName = "Smith", dateOfBirth = "01-Jan-1980" };
+			consumerDetails[1] = new ConsumerDetails { firstName = "Steve", lastName = "Jobs", dateOfBirth = "22-Dec-1970" };
+
+			string response = JsonConvert.SerializeObject(consumerDetails, Formatting.None);
+
+			return  response ;
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return "Welcome to ADS Info Systems, A new venture of advance technologies in digital world";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ GCP_ConsumerApplication/Models/ConsumerDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GCP_ConsumerApplication.Models
+{
+	public class ConsumerDetails
+	{
+		public string firstName;
+		public string lastName;
+		public string dateOfBirth;
+	}
+}
